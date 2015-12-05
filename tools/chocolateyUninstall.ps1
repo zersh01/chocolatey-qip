4ced68d011df04c490714c25537b39bb4f800af9
zersh01 <zersh@mail.ru>
2015-12-05 20:22:40 +0300

    Update Icon

---
+++ tools/chocolateyUninstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = 'qip2012'
+$fileType = 'exe'
+$silentArgs = '/VERYSILENT /NORESTART'
+ 
+$uninstallString = (Get-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\QIP 2012').UninstallString
+
+if ($uninstallString -ne "") {
+    Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $uninstallString
+}
+
